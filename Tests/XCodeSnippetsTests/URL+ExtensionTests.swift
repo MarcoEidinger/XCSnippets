7fa83a43e758c80413ff3a0deeb42d092479f821
Marco Eidinger <eidingermarco@gmail.com>
2022-06-12 06:04:33 -0700

    add unit test

---
+++ Tests/XCodeSnippetsTests/URL+ExtensionTests.swift
@@ -0,0 +1,10 @@
+@testable import XCodeSnippets
+import XCTest
+
+final class URLTests: XCTestCase {
+    func testCodeSnippetsUserDirectoryURL() throws {
+        let sut = try URL.codeSnippetsUserDirectoryURL
+        XCTAssertTrue(sut.absoluteString.hasPrefix("file:///Users/"), "\(sut.absoluteString) not as expected")
+        XCTAssertTrue(sut.absoluteString.hasSuffix("Library/Developer/Xcode/UserData/CodeSnippets/"), "\(sut.absoluteString) not as expected")
+    }
+}
